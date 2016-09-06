68f69bec7451cb43f26e880b0d04ee763d8d09db
geekjob-support <geekjob_support@groovegear.jp>
2016-09-06 17:11:31 +0900

    基礎の課題

---
+++ 001.プログラミング基礎学習１/kadai4,5.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+    $hensu=6;
+    const teisu=2;
+    echo $hensu+teisu;   //6+2
+    echo '<br>';
+    echo $hensu-teisu;   //6-2
+    echo '<br>';
+    echo $hensu*teisu;   //6*2
+    echo '<br>';
+    echo $hensu/teisu;   //6/2?>
