87f83747bdf1a7345e648e9f66edadb356e35089
geekjob-support <geekjob_support@groovegear.jp>
2016-09-09 14:02:30 +0900

    9/9

---
+++ 002.プログラミング基礎学習２/kiso2kadai10.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+$teisu=$_GET['teisu'];
+$sosu=array(2,3,5,7);
+$a='a';
+$b='b';
+$soinsu=array();
+$hairetu=0;
+
+echo '元の数 '.$teisu.'<br>';
+
+while($teisu!=1){//元の数が１になるまで素因数分解
+if($teisu==0){//元の数が０なら終わり
+break;}
+if($a==$b){//１桁以外の素因数がある場合終わり
+break;}
+$a=$b;//１桁以外の素因数判別用
+foreach($sosu as $value){//素因数分解
+if($teisu%$value!=null){
+continue;}
+$soinsu[$hairetu]=$value;//素因数の格納
+$hairetu=$hairetu+1;
+$teisu=$teisu/$value;
+$a=$teisu;}//一桁以外の素因数判別用
+}
+echo '一桁の素因数 ';
+foreach($soinsu as $value){echo $value.',';}//一桁の素因数の表示
+if($teisu>1){echo '<br>'.'その他 '.$teisu;}
---
+++ 003.プログラミング基礎学習３/util.php
@@ -0,0 +1,12 @@
+﻿<?php
+function my_profile(){
+echo '今別府侑馬<br>';
+echo '1991年9月14日生まれ<br>';
+echo '趣味はゲームです。<br>';
+}
+function gusukisu($hanbetu){
+if($hanbetu%2==0){
+echo $hanbetu.'は偶数です';}
+else{
+echo $hanbetu.'は奇数です';}
+}
